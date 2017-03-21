bc7e98d311cb6963accd973a27c3ac2b69e72c49
Anastasiia Zolochevska <anastasiia.zolochevska@gmail.com>
2017-03-21 14:36:42 -0700

    fix cors

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var nodeStatic = require('node-static');
 var http = require('http');
 var socketIO = require('socket.io');
 
- var port = process.env.PORT || 1234;
+ var port = process.env.PORT || 80;
  var app = http.createServer(function(req, res) {
    res.writeHead(200, { 'Content-Type': 'text/plain'});
    res.end('Signaling server\n');
