6187eed00c182789481a2800acc04fb5f339e8a4
Anastasia Zolochevska <anastasiia.zolochevska@gmail.com>
2017-05-26 15:27:36 -0700

    Http based signaling server comapible with native client

--- server.js
+++ server.js
@@ -1,49 +1,146 @@
 
-'use strict';
+var express = require('express')
+var bodyParser = require('body-parser')
 
-var os = require('os');
-var nodeStatic = require('node-static');
-var http = require('http');
-var socketIO = require('socket.io');
+var clientCounter = 1;
+var clientToId = {};
+var generalRoom = "GENERAL";
+var peers = {};
 
-var port = process.env.PORT || 80;
-var app = http.createServer(function (req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Signaling server\n');
-}).listen(port);
+var port = process.env.PORT || 3000;
 
 
-var appInsightsInstrumentationKey = "66eb311b-f091-41dc-8ddb-d8b1647091d3"
-var appInsights = require("applicationinsights");
-appInsights.setup(appInsightsInstrumentationKey).setAutoCollectExceptions(true).start();
-var appInsightsClient = appInsights.getClient();
+var app = express();
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.text())
+
+
+app.get('/sign_in', function (req, res) {
+    var client = {};
+    console.log(req.url);
+    var newPeer = {}
+    newPeer.id = clientCounter++;
+    newPeer.peerType = 'client';
+    newPeer.messages = [];
+    newPeer.name = req.url.substring(req.url.indexOf("?") + 1, req.url.length - 1);
+    if (newPeer.name.indexOf("renderingclient_") != -1) {
+        newPeer.peerType = 'client';
+    }
+    if (newPeer.name.indexOf("renderingserver_") != -1) {
+        newPeer.peerType = 'server';
+    }
+    peers[newPeer.id] = newPeer;
+
+    res.set('Pragma', newPeer.id);
+    res.send(formatListOfPeers(newPeer));
+    notifyOtherPeers(newPeer);
+})
 
 
 
+app.post('/message', function (req, res) {
+    console.log(req.url);
+    console.log(req.body);
+    console.log(req.headers['content-length']);
+    var fromId = req.query.peer_id;
+    var toId = req.query.to;
+    var payload = req.body;
+    var contentLength = req.headers['content-length'];
+    contentLength = parseInt(contentLength);
+    if (!peers[toId] || !peers[fromId]) {
+        res.status(400).send();
+    }
+    if (contentLength <= payload.length) {
+        peers[toId].roomPeer = peers[fromId]
+        peers[fromId].roomPeer = peers[toId];
+        sendMessageToPeer(peers[toId], payload, fromId);
+        res.set('Pragma', fromId);
+        res.send();
+    }
 
-var io = socketIO.listen(app);
-io.sockets.on('connection', function (socket) {
+})
 
-  // convenience function to log server messages on the client
-  function log() {
-    var array = ['Message from server:'];
-    array.push.apply(array, arguments);
-    console.log(array);
-    socket.emit('log', array);
-    appInsightsClient.trackTrace(array.join(" "));
-  }
+app.get('/sign_out', function (req, res) {
+    console.log(req.url);
+    var peerId = req.query.peer_id;
+    var peer = peers[peerId]
+    delete peers[peerId]
 
-  socket.on('message', function (message, room) {
-    log('Client said: ', message);
-    io.to(room).emit('message', message);
-  });
+    if (peer.roomPeer) {
+        peer.roomPeer.roomPeer = null;
+        peer.roomPeer = null;
+    }
+    res.set('Pragma', peerId);
+    res.send();
+})
 
-  socket.on('join', function (room) {
-    socket.join(room);
-  });
 
-  socket.on('bye', function (room) {
-    socket.leave(room)
-  });
+app.get('/wait', function (req, res) {
+    console.log(req.url);
+    var peerId = req.query.peer_id;
+    var socket = {};
+    socket.waitPeer = peers[peerId];
+    socket.res = res;
+    peers[peerId].waitSocket = socket;
+    sendMessageToPeer(peers[peerId], null, null);
+})
 
-});
+
+function formatListOfPeers(peer) {
+    var result = peer.name + "," + peer.id + ",1\n";
+    for (peerId in peers) {
+        var otherPeer = peers[peerId];
+        if (isPeerCandidate(peer, otherPeer)) {
+            result += otherPeer.name + "," + otherPeer.id + ",1\n"
+        }
+    }
+    return result;
+}
+
+function log(message) {
+    console.log(message);
+    client.trackTrace(message);
+}
+
+function notifyOtherPeers(newPeer) {
+    for (peerId in peers) {
+        var otherPeer = peers[peerId];
+        if (isPeerCandidate(newPeer, otherPeer)) {
+            var data = newPeer.name + "," + newPeer.id + ",1\n";
+            sendMessageToPeer(otherPeer, data);
+        }
+    }
+}
+
+function sendMessageToPeer(peer, payload, fromId) {
+    var msg = {};
+    if (payload) {
+        msg.id = fromId || peer.id;
+        msg.payload = payload;
+        peer.messages.push(msg);
+    }
+    if (peer.waitSocket) {
+        msg = peer.messages.shift();
+        if (msg) {
+            peer.waitSocket.res.set('Pragma', msg.id);
+            peer.waitSocket.res.send(msg.payload);
+            peer.waitSocket.waitPeer = null;
+            peer.waitSocket.tmpData = "";
+            peer.waitSocket = null;
+        }
+    }
+}
+
+function isPeerCandidate(peer, otherPeer) {
+    return (otherPeer.id != peer.id && // filter self
+        !otherPeer.roomPeer && // filter peers in 'rooms'
+        otherPeer.peerType != peer.peerType) // filter out peers of same type
+}
+
+
+app.listen(port)
+
